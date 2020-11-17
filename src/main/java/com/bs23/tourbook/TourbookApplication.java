bda8fe2f6efb80e67fd8d6a38d18cf5167401485
Ahmed Dinar <ahmed.dinar@brainstation23.com>
2020-11-17 23:06:27 +0600

    first commit

---
+++ src/main/java/com/bs23/tourbook/TourbookApplication.java
@@ -0,0 +1,13 @@
+package com.bs23.tourbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourbookApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TourbookApplication.class, args);
+  }
+
+}
